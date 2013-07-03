f94ec9aab94007b0f6fe8a1930a3fdb1f8f52f0c
wagnert <t.wagner@techdivision.com>
2013-07-03 15:18:31 +0200

    Refactoring package creation for mac os x

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -6,16 +6,23 @@ define('DS', DIRECTORY_SEPARATOR);
 define('PS', PATH_SEPARATOR);
 define('BP', __DIR__);
 
+// load composer namespaces and add them to the include path
+$paths = array();
+$namespaces = require __DIR__ . '/app/code/vendor/composer/autoload_namespaces.php';
+foreach ($namespaces as $namespace) {
+    $paths = array_merge($paths, $namespace);
+}
+
+// add the default include paths
 $paths[] = __DIR__ . DS . 'app' . DS . 'code' . DS . 'local';
 $paths[] = __DIR__ . DS . 'app' . DS . 'code' . DS . 'community';
 $paths[] = __DIR__ . DS . 'app' . DS . 'code' . DS . 'core';
 $paths[] = __DIR__ . DS . 'app' . DS . 'code' . DS . 'lib'; // directory for PEAR libraries
-$paths[] = __DIR__ . DS . 'app' . DS . 'code' . DS . 'vendor'; // directory for Composer libraries
 
 // set the new include path
 set_include_path(implode(PS, $paths) . PS . get_include_path());
 
+// register class loader again, because we are in a thread
 require 'TechDivision/SplClassLoader.php';
-
 $classLoader = new SplClassLoader();
-$classLoader->register();
+$classLoader->register(true);
