e40ce8b423e0cc45f108eaa8ce12d1ee3c40f6ed
zelgerj <zelger@me.com>
2013-08-21 17:30:18 +0200

    refactored path constants in bootstrapper

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,23 +1,23 @@
 <?php
 
-define('DS', DIRECTORY_SEPARATOR);
-define('PS', PATH_SEPARATOR);
-define('BP', __DIR__);
+define('APPSERVER_DS', DIRECTORY_SEPARATOR);
+define('APPSERVER_PS', PATH_SEPARATOR);
+define('APPSERVER_BP', __DIR__);
 
 // load composer namespaces and add them to the include path
 $paths = array();
-$namespaces = require __DIR__ . '/app/code/vendor/composer/autoload_namespaces.php';
+$namespaces = require APPSERVER_BP . '/app/code/vendor/composer/autoload_namespaces.php';
 foreach ($namespaces as $namespace) {
     $paths = array_merge($paths, $namespace);
 }
 
 // add the default include paths
-$paths[] = __DIR__ . DS . 'app' . DS . 'code' . DS . 'local';
-$paths[] = __DIR__ . DS . 'app' . DS . 'code' . DS . 'community';
-$paths[] = __DIR__ . DS . 'app' . DS . 'code' . DS . 'core';
-$paths[] = __DIR__ . DS . 'app' . DS . 'code' . DS . 'lib'; // directory for PEAR libraries
+$paths[] = APPSERVER_BP . APPSERVER_DS . 'app' . APPSERVER_DS . 'code' . APPSERVER_DS . 'local';
+$paths[] = APPSERVER_BP . APPSERVER_DS . 'app' . APPSERVER_DS . 'code' . APPSERVER_DS . 'community';
+$paths[] = APPSERVER_BP . APPSERVER_DS . 'app' . APPSERVER_DS . 'code' . APPSERVER_DS . 'core';
+$paths[] = APPSERVER_BP . APPSERVER_DS . 'app' . APPSERVER_DS . 'code' . APPSERVER_DS . 'lib'; // directory for PEAR libraries
 
 // set the new include path
-set_include_path(get_include_path() . PS . implode(PS, $paths));
+set_include_path(get_include_path() . APPSERVER_PS . implode(APPSERVER_PS, $paths));
 
-require __DIR__ . '/app/code/vendor/autoload.php';
+require APPSERVER_BP . '/app/code/vendor/autoload.php';
