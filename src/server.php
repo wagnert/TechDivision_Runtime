836cf5bb835937f47cea5e09983ba53ac4b9bf7a
wagnert <t.wagner@techdivision.com>
2013-08-23 18:43:45 +0200

    Add initial context configuration

--- src/server.php
+++ src/server.php
@@ -14,7 +14,12 @@ ini_set('max_execution_time', 0);
 
 // bootstrap the application
 require __DIR__ . DIRECTORY_SEPARATOR . 'bootstrap.php';
+        
+// initialize the SimpleXMLElement with the content XML configuration file
+$configuration = new Configuration();
+$configuration->initFromFile(APPSERVER_BP . DIRECTORY_SEPARATOR . 'etc' . DIRECTORY_SEPARATOR . 'appserver.xml');
+$configuration->addChildWithNameAndValue('baseDirectory', APPSERVER_BP);
 
 // start the server finally
-$server = new Server();
+$server = new Server($configuration);
 $server->start();
