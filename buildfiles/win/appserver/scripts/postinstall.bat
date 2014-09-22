3ae4ef8767ac1e00f62d737badda0841db6044d4
Tim Wagner <t.wagner@techdivision.com>
2014-09-22 09:57:46 +0200

    Remove Windows postinstall.bat from IZPack installer

--- buildfiles/win/appserver/scripts/postinstall.bat
+++
@@ -1,8 +0,0 @@
-@ECHO OFF
-ECHO Executing postinstall script for appserver.io...
-
-set php=php\php.exe
-set ini=php\php.ini
-set composer=bin\composer.phar
-
-%php% -c %ini% %composer% run-script post-install-cmd
